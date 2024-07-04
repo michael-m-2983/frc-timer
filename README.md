9887130e1232db11dc55ec2f8e8309a19030386b
Colten Kiehne <cokiehne@gmail.com>
2024-07-03 19:45:16 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+## FRC-Timer
+
+This is a Timer designed for FRC matches (mainly for summer camp use), but can be extended for anything needing a timer and points system.
+
+Use the buttons below the timer circle to start/stop the timer, reset the timer, and upload matches.
+
+<img src="./images/example.png">
+
+---
+
+Matches must be done in JSON formatting, which [matches.json](./matches.json) has an example for. Use the upload button to load your match files, formatted as in the example.
+
+```json
+{
+    "m1" : {
+        "red": "Alliance 1",
+        "blue": "Alliance 2"
+    },
+    ...
+}
+```
+The names provided in each match will be the names displayed for each alliance color in the current score, and also in the final score reveal.
+
+Resetting the timer after a match (with matches data uploaded) has finished will result in the next match being brought up. This process will continue until there are no matches left, in which case no more matches will be played.
+
+---
+
+Once a match has finished, (when the timer has counted down to zero), clicking the 'r' key on the keyboard will reveal the final scores and the winner. Clicking the 'r' key again will toggle the final scores off. Note that any changes to scoring will not reflect on the final scores unless they are toggled off and back on after being revealed.
+
+<img src="./images/final-reveal.png">
+
+---
+
+Scoring uses the 1-8 keys on the keyboard, each pair containing an increment and decrement for points and penalties. So clicking 1 will give 1 point to red while clicking 2 will take the point back off.
+
+The way that we have used the scoring feature is with [micro:bit](https://microbit.org/) controllers acting as bluetooth keyboard, and their buttons acting as the respective keys, which makes scoring with multiple much simpler. 
+
+- 1/2 - Increment/Decrement Red Score
+- 3/4 - Increment/Decrement Blue Score
+- 5/6 - Increment/Decrement Red Penalties
+- 7/8 - Increment/Decrement Blue Penalties
---
+++ script.js
@@ -0,0 +1,306 @@
+const timeDisplay = document.getElementById("timer-display");
+const displayCircle = document.getElementById("timer-circle");
+const redDisplay = document.getElementById("red-score");
+const blueDisplay = document.getElementById("blue-score");
+const redReveal = document.getElementById("red-reveal");
+const blueReveal = document.getElementById("blue-reveal");
+const fileInput = document.getElementById("file-input");
+
+// Button keymap
+KEYMAP = [
+  ["s", 0, 1],
+  ["s", 0, -1],
+  ["s", 1, 1],
+  ["s", 1, -1],
+  ["p", 0, 1],
+  ["p", 0, -1],
+  ["p", 1, 1],
+  ["p", 1, -1],
+];
+
+var matchesJSON = null;
+var matchNumber = 1;
+
+// Red, blue
+var points = [0, 0];
+var penalties = [0, 0];
+
+// load matches if "use" is set to true
+window.onload = function () {
+  fetch("./matches.json")
+    .then((text) => text.text())
+    .then((json) => JSON.parse(json))
+    .then((json) => {
+      if (json.use == true) {
+        matchesJSON = json;
+        loadMatch(matchNumber);
+      }
+    });
+};
+
+fileInput.onchange = function () {
+  reader = new FileReader();
+  reader.readAsText(fileInput.files[0]);
+
+  reader.onload = function () {
+    try {
+      matchesJSON = JSON.parse(reader.result);
+      matchNumber = 1;
+      loadMatch(matchNumber);
+    } catch (error) {
+      alert("Unable to parse file!");
+    }
+    
+  };
+};
+
+document.addEventListener("keyup", (event) => {
+  if (event.key == "r" && timePassed == initialTime) {
+    toggleScores();
+  } else if ("12345678".includes(event.key) && timePassed != 0) {
+    let action = KEYMAP[parseInt(event.key) - 1];
+    console.log(`${event.key}: ${action}`);
+    if (action[0] == "s") {
+      points[action[1]] = Math.max(0, points[action[1]] + action[2]);
+    } else {
+      penalties[action[1]] = Math.max(0, penalties[action[1]] + action[2]);
+    }
+
+    updateScore(false, points[0]);
+    updateScore(true, points[1]);
+
+    updatePenalties(false, penalties[0]);
+    updatePenalties(true, penalties[1]);
+  }
+});
+
+// seconds for timer
+var initialTime = 135;
+timeDisplay.innerHTML = formatDisplayTime(initialTime);
+
+var timePassed = 0;
+var timerInterval = null;
+
+var redTitle = document.createElement("span");
+redTitle.innerHTML = "Red";
+var blueTitle = document.createElement("span");
+blueTitle.innerHTML = "Blue";
+
+redDisplay.appendChild(redTitle);
+blueDisplay.appendChild(blueTitle);
+
+document.addEventListener("keyup", (event) => {});
+
+function formatDisplayTime(time) {
+  var minutes = Math.floor(time / 60);
+  var seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+function updateDisplayTime() {
+  timeDisplay.innerHTML = formatDisplayTime(timeRemaining);
+  --timeRemaining;
+}
+
+function startTimer() {
+  timeDisplay.setAttributeNS(null, "fill", "white");
+  if (timePassed == 0) {
+    new Audio("sounds/start.mp3").play();
+  }
+
+  if (timePassed != initialTime) {
+    if (timerInterval == null) {
+      timePassed += 1;
+      timeDisplay.innerHTML = formatDisplayTime(initialTime - timePassed);
+      changeCirclePercent();
+
+      timerInterval = setInterval(() => {
+        timePassed += 1;
+
+        timeDisplay.innerHTML = formatDisplayTime(initialTime - timePassed);
+
+        if (timePassed == initialTime) {
+          stopTimer();
+        }
+
+        changeCirclePercent();
+
+        if (initialTime - timePassed == 30) {
+          new Audio("sounds/whistle.mp3").play();
+        } else if (initialTime - timePassed == 0) {
+          new Audio("sounds/endbuzzer.mp3").play();
+        }
+
+        if (initialTime - timePassed <= 10) {
+          displayCircle.classList = "circle-red";
+          timeDisplay.classList.add("timer-end");
+          void timeDisplay.offsetWidth;
+        } else if (initialTime - timePassed <= 30) {
+          displayCircle.classList = "circle-orange";
+        } else {
+          displayCircle.classList = "circle-green";
+        }
+      }, 1000);
+    }
+  }
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
+  if (!timeDisplay.classList.contains("timer-end")) {
+    timeDisplay.setAttributeNS(null, "fill", "yellow");
+  }
+}
+
+function changeCirclePercent() {
+  var rawTimeFraction = 1 - timePassed / initialTime;
+  circleDashArray = (rawTimeFraction * 629).toFixed(0) + " 628";
+  displayCircle.setAttributeNS(null, "stroke-dasharray", circleDashArray);
+}
+
+function resetTimer() {
+  stopTimer();
+
+  // if no loaded matches, timer is finished, and there are more matches to run
+  if (
+    matchesJSON != null &&
+    timePassed == initialTime &&
+    Object.keys(matchesJSON).join().match(/m/g).length > matchNumber
+  ) {
+    loadMatch(++matchNumber);
+  }
+
+  points = [0, 0];
+  penalties = [0, 0];
+
+  setTimeout(() => {
+    updateScore(true, 0);
+    updateScore(false, 0);
+
+    updatePenalties(false, 0);
+    updatePenalties(true, 0);
+  }, 3000);
+
+  timePassed = 0;
+  displayCircle.setAttributeNS(null, "stroke-dasharray", "629 628");
+  timeDisplay.classList.remove("timer-end");
+  timeDisplay.setAttributeNS(null, "fill", "white");
+  void timeDisplay.offsetWidth;
+  displayCircle.classList = "circle-green";
+  timeDisplay.innerHTML = formatDisplayTime(initialTime);
+}
+
+function updateScore(isBlue, score) {
+  if (isBlue) {
+    blueDisplay.innerHTML = "";
+    blueDisplay.appendChild(blueTitle);
+    blueDisplay.innerHTML += score;
+  } else {
+    redDisplay.innerHTML = "";
+    redDisplay.appendChild(redTitle);
+    redDisplay.innerHTML += score;
+  }
+}
+
+function updatePenalties(isBlue, penalties) {
+  if (isBlue) {
+    document.getElementById("blue-penalties").innerHTML = penalties;
+  } else {
+    document.getElementById("red-penalties").innerHTML = penalties;
+  }
+}
+
+function toggleTeams(resetScores) {
+  if (redDisplay.classList.contains("red-score-enter")) {
+    blueDisplay.classList.remove("blue-score-enter");
+    redDisplay.classList.remove("red-score-enter");
+    blueDisplay.classList.add("blue-score-exit");
+    redDisplay.classList.add("red-score-exit");
+  } else {
+    blueDisplay.classList.remove("blue-score-exit");
+    redDisplay.classList.remove("red-score-exit");
+    blueDisplay.classList.add("blue-score-enter");
+    redDisplay.classList.add("red-score-enter");
+  }
+
+  if (resetScores) {
+    updateScore(true, 0);
+    updateScore(false, 0);
+  }
+}
+
+function toggleScores() {
+  if (redReveal.classList.contains("red-reveal-enter")) {
+    blueReveal.classList.remove("blue-reveal-enter");
+    redReveal.classList.remove("red-reveal-enter");
+    blueReveal.classList.add("blue-reveal-exit");
+    redReveal.classList.add("red-reveal-exit");
+
+    if (redReveal.children[0].id == "tied") {
+      redReveal.removeChild(redReveal.firstChild);
+    }
+  } else {
+    // update scores in reveal divs
+    document.getElementById("red-reveal-points").innerHTML =
+      points[0] - penalties[0];
+    document.getElementById("red-reveal-penalties").innerHTML = penalties[0];
+    document.getElementById("blue-reveal-points").innerHTML =
+      points[1] - penalties[1];
+    document.getElementById("blue-reveal-penalties").innerHTML = penalties[1];
+
+    winnerDiv = document.createElement("div");
+    winnerDiv.id = "winner";
+    winnerDiv.innerHTML = "Winner!";
+
+    if (redReveal.children.length == 2) {
+      redReveal.removeChild(redReveal.firstChild);
+    }
+
+    if (blueReveal.children.length == 2) {
+      blueReveal.removeChild(blueReveal.firstChild);
+    }
+
+    // Winner logic
+    if (points[0] - penalties[0] > points[1] - penalties[1]) {
+      redReveal.insertBefore(winnerDiv, redReveal.firstChild);
+    } else if (points[0] - penalties[0] < points[1] - penalties[1]) {
+      blueReveal.insertBefore(winnerDiv, blueReveal.firstChild);
+    } else if (points[0] - penalties[0] == points[1] - penalties[1]) {
+      if (penalties[0] < penalties[1]) {
+        redReveal.insertBefore(winnerDiv, redReveal.firstChild);
+      } else if (penalties[0] > penalties[1]) {
+        blueReveal.insertBefore(winnerDiv, blueReveal.firstChild);
+      } else {
+        winnerDiv.id = "tied";
+        winnerDiv.innerHTML = "Tied!";
+        redReveal.insertBefore(winnerDiv, redReveal.firstChild);
+      }
+    }
+
+    blueReveal.classList.remove("blue-reveal-exit");
+    redReveal.classList.remove("red-reveal-exit");
+    blueReveal.classList.add("blue-reveal-enter");
+    redReveal.classList.add("red-reveal-enter");
+  }
+}
+
+function loadMatch(matchNumber) {
+  matchData = matchesJSON["m" + matchNumber];
+  redTitle.innerHTML = matchData.red;
+  blueTitle.innerHTML = matchData.blue;
+
+  document.getElementById("red-reveal-name").innerHTML = matchData.red;
+  document.getElementById("blue-reveal-name").innerHTML = matchData.blue;
+
+  toggleTeams();
+
+  setTimeout(function () {
+    toggleTeams(true);
+  }, 2000);
+}
